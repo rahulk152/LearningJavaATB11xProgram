6a06e22b229ac1858ab64b201562aea7953bfe87
rahul <rahulkumaraiet@yahoo.in>
2025-03-10 09:15:03 +0530

    10 March 2025

---
+++ src/ex_01_Java_Basic/Lab003_Keyword_Identifiers.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+//package -> It's keyword
+// ex_01_Java_Basic - Identifier
+public class Lab003_Keyword_Identifiers {
+    //public -> Keyword
+    //class -> keyword
+    //Lab003_Keyword_Identifiers - Identifier
+    public static void main(String[] args) {
+        //public - keyword
+//        staic-> keyword
+//        void -> keyword
+        System.out.println("Keyword and Identifiers");
+
+    }
+}
