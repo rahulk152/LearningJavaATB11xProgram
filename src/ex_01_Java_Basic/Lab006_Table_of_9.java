99b4d889623460e99257fc91bede6de651911738
rahul <rahulkumaraiet@yahoo.in>
2025-03-11 08:58:30 +0530

    11 March 2025

--- src/ex_01_Java_Basic/Lab006_Table_of_9.java
+++ src/ex_01_Java_Basic/Lab006_Table_of_9.java
@@ -6,25 +6,25 @@ public class Lab006_Table_of_9 {
     public static void main(String[] args) {
         byte table = 9;
         System.out.println("Table of 9 is:");
-        System.out.printf("%d * %d = %d",table,1,(table*1));
+        System.out.printf("%d * 1 = %d",table,(table));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,2,(table*2));
+        System.out.printf("%d * 2 = %d",table,(table*2));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,3,(table*3));
+        System.out.printf("%d * 3 = %d",table,(table*3));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,4,(table*4));
+        System.out.printf("%d * 4 = %d",table,(table*4));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,5,(table*5));
+        System.out.printf("%d * 5 = %d",table,(table*5));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,6,(table*6));
+        System.out.printf("%d * 6 = %d",table,(table*6));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,7,(table*7));
+        System.out.printf("%d * 7 = %d",table,(table*7));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,8,(table*8));
+        System.out.printf("%d * 8 = %d",table,(table*8));
         System.out.println();
-        System.out.printf("%d * %d = %d",table,9,(table*9));
+        System.out.printf("%d * 9 = %d",table,(table*9));
         System.out.println();
-       System.out.printf("%d * %d = %d",table,10,(table*10));
+       System.out.printf("%d * 10 = %d",table,(table*10));
 
     }
 }
