f417f00c188e13db2a0ecdd3dff7f9b2ba782506
rahul <rahulkumaraiet@yahoo.in>
2025-03-11 08:47:11 +0530

    11 March 2025

---
+++ src/ex_01_Java_Basic/Lab005_Variables.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Variables {
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = 787;
+        short s = 45;
+        long l = 7876451454L;
+        float f = 3.14f;
+        double d = 846545343;
+        int g =10, h=45, o=56;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab006_Table_of_9.java
@@ -0,0 +1,30 @@
+package ex_01_Java_Basic;
+
+import java.io.PrintStream;
+
+public class Lab006_Table_of_9 {
+    public static void main(String[] args) {
+        byte table = 9;
+        System.out.println("Table of 9 is:");
+        System.out.printf("%d * %d = %d",table,1,(table*1));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,2,(table*2));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,3,(table*3));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,4,(table*4));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,5,(table*5));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,6,(table*6));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,7,(table*7));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,8,(table*8));
+        System.out.println();
+        System.out.printf("%d * %d = %d",table,9,(table*9));
+        System.out.println();
+       System.out.printf("%d * %d = %d",table,10,(table*10));
+
+    }
+}
