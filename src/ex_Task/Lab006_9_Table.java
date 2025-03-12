acb9bb445623bc2867cbdaba9c1e23261b757760
rahul <rahulkumaraiet@yahoo.in>
2025-03-12 09:19:31 +0530

    12 March 2025

---
+++ src/ex_Task/Lab006_9_Table.java
@@ -0,0 +1,18 @@
+package ex_Task;
+
+public class Lab006_9_Table {
+    public static void main(String[] args) {
+        byte table = 9;
+        System.out.println("table of 9 is:");
+        System.out.printf("%d * 1 = %d\n", table,(table));
+        System.out.printf("%d * 2 = %d\n", table,(table*2));
+        System.out.printf("%d * 3 = %d\n", table,(table*3));
+        System.out.printf("%d * 4 = %d\n", table,(table*4));
+        System.out.printf("%d * 5 = %d\n", table,(table*5));
+        System.out.printf("%d * 6 = %d\n", table,(table*6));
+        System.out.printf("%d * 7 = %d\n", table,(table*7));
+        System.out.printf("%d * 8 = %d\n", table,(table*8));
+        System.out.printf("%d * 9 = %d\n", table,(table*9));
+        System.out.printf("%d * 10 = %d\n", table,(table*10));
+    }
+}
