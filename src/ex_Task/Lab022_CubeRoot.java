5d67a625d29efbca30bda1b10927655e6affa9ce
rahkumar <rahulkumaraiet@yahoo.in>
2025-03-12 10:00:17 +0530

    12 March 2025

---
+++ src/ex_Task/Lab022_CubeRoot.java
@@ -0,0 +1,9 @@
+package ex_Task;
+
+public class Lab022_CubeRoot {
+    public static void main(String[] args) {
+       int X = 10, Y = 10, Z = 10;
+      double result = Math.cbrt((X*X)+(Y*Y)-Math.abs(Z));
+        System.out.println(result);
+    }
+}
