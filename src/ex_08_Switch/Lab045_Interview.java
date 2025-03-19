4a4772963d8a14fcf5f21b5bff6b788d44f2c2e4
rahkumar <rahulkumaraiet@yahoo.in>
2025-03-19 10:34:21 +0530

    19 March 2025

---
+++ src/ex_08_Switch/Lab045_Interview.java
@@ -0,0 +1,55 @@
+package ex_08_Switch;
+
+public class Lab045_Interview {
+    public static void main(String[] args) {
+        int a = 10;
+        switch (a) {
+
+        }
+ //       boolean b = true;
+//        switch (b) {
+//        }
+        char ch = 'A';
+        switch (ch){
+            case 65:
+                System.out.println("Match ASCII");
+                break;
+            default:
+                System.out.println("No Match");
+        }
+        long a11 = 30l;
+        switch ((int) a11) {
+        }
+        int c = 98;
+        switch (c) {
+            case 98:
+                System.out.println("98");
+//            case 98:
+//                System.out.println("98");
+        }
+        int d = 11;
+        switch (-1){
+            default:
+                System.out.println("Default");
+                break;
+            case -1:
+                System.out.println("10");
+                break;
+            case 9:
+                System.out.println("9");
+                break;
+        }
+        char code = 'C';
+        switch (code){
+            default:
+                System.out.println("Hellooooooo");
+                break;
+            case 'A':
+                System.out.println("65");
+                break;
+            case 'B':
+                System.out.println("66");
+                break;
+        }
+    }
+}
---
+++ src/ex_09_For_Loop/Lab051_For_If_Loop.java
@@ -0,0 +1,14 @@
+package ex_09_For_Loop;
+
+public class Lab051_For_If_Loop {
+    public static void main(String[] args) {
+        for(int i = 0; i < 18; i++){
+            if(i > 15){
+                System.out.println("You will get gift of iPhone");
+            }
+            else{
+                System.out.println("No gift");
+            }
+        }
+    }
+}
