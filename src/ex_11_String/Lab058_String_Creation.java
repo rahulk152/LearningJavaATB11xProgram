491074893c81fc427a0b110403a1ef67e45e2712
rahkumar <rahulkumaraiet@yahoo.in>
2025-03-24 10:14:40 +0530

    24 March 2025

---
+++ src/ex_11_String/Lab058_String_Creation.java
@@ -0,0 +1,11 @@
+package ex_11_String;
+
+public class Lab058_String_Creation {
+    public static void main(String[] args) {
+        // String 2 ways
+        String s1 = "pramod"; // SCP
+
+        String s2 = new String("pramod"); // OA -> Object Area
+
+    }
+}
