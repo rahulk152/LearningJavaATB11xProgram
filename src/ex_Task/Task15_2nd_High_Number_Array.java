4f06f543b0077ce76ab97f8694b59f1d8208060a
rahkumar <rahulkumaraiet@yahoo.in>
2025-03-27 15:57:56 +0530

    27 March 2025

--- src/ex_Task/Task15_2nd_High_Number_Array.java
+++ src/ex_Task/Task15_2nd_High_Number_Array.java
@@ -4,21 +4,21 @@ public class Task15_2nd_High_Number_Array {
     public static void main(String[] args) {
         int[] number = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
         int max = 0;
-        int se_max = 0;
+        int second_largest = 0;
 
         for (int i = 0; i < number.length; i++) {
             if (number[i] > max) {
-            
-                se_max = max;
+
+                second_largest = max;
                 max = number[i];
 
-            } else if (number[i] > se_max && number[i] != max) {
-                se_max = number[i];
+            } else if (number[i] > second_largest && number[i] != max) {
+                second_largest = number[i];
             }
 
         }
         System.out.println("Max number is " +max);
-        System.out.println("Second max number is " +se_max);
+        System.out.println("Second max number is " +second_largest);
 
     }
 
