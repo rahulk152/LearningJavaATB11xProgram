2eb64560d3f503e0f8d1daa3a5245c1d2955ce0e
rahkumar <rahulkumaraiet@yahoo.in>
2025-04-01 17:05:21 +0530

    01 April 2025

---
+++ src/ex_OOPS_Extra/Task01_Default_Constructor.java
@@ -0,0 +1,14 @@
+package ex_OOPS_Extra;
+
+public class Task01_Default_Constructor {
+    public static void main(String[] args) {
+        car c1 = new car();
+
+    }
+}
+class car{
+
+    car(){
+        System.out.println("Default Constructor");
+    }
+}
