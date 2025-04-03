76d2e1c3d163479108f8692c7dc5dd3c0632879c
rahkumar <rahulkumaraiet@yahoo.in>
2025-04-03 10:28:24 +0530

    03 April 2025

---
+++ src/ex_20_OOPs_Polymorphism/Overriding/Lab101_Method_Overriding.java
@@ -0,0 +1,21 @@
+package ex_20_OOPs_Polymorphism.Overriding;
+
+public class Lab101_Method_Overriding {
+    public static void main(String[] args) {
+        dog d = new dog();
+        d.sound();
+        Animal a = new dog(); //Dynamic Dispatch
+        a.sound();
+    }
+}
+class Animal{
+    void sound(){
+        System.out.println("Animal Sound!");
+    }
+}
+class dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("Bark!");
+    }
+}
