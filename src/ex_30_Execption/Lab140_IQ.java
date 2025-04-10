6f4aeb1b2bf953a24126431df5100d31bd6494be
rahul <rahulkumaraiet@yahoo.in>
2025-04-10 08:36:48 +0530

    10 April 2025

---
+++ src/ex_30_Execption/Lab140_IQ.java
@@ -0,0 +1,14 @@
+package ex_30_Execption;
+
+public class Lab140_IQ {
+    public static void main(String[] args) {
+        try {
+            String input_user  = args[0]; // java.lang.ArrayIndexOutOfBoundsExceptio
+            int a = Integer.parseInt(input_user); // java.lang.NumberFormatException: For input string
+            int output = 100/a;//
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException | ArithmeticException e) {
+            // new NumberFormatException (); JVM is creating the object
+            System.out.println(e.getMessage());
+        }
+    }
+}
