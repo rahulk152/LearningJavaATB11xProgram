48eb63368ee47a22745f96cf2d04815918832362
rahkumar <rahulkumaraiet@yahoo.in>
2025-07-08 17:38:36 +0530

    8 July 2025

--- src/Interview_program_code/Number/Task_31_Numeric_Hollow_Pyramid.java
+++ src/Interview_program_code/Number/Task_31_Numeric_Hollow_Pyramid.java
@@ -7,7 +7,7 @@ public interface Task_31_Numeric_Hollow_Pyramid {
         for (int row = 1; row <= totalNumberOfRows; row++) {
 
             for (int col = 1; col <= row; col++) {
-                if (col == 1 || col == row || row == totalNumberOfRows){
+                if (col == 1 || col == row  || row == totalNumberOfRows){
                     System.out.print(col);
                 }
                 System.out.print(" ");
